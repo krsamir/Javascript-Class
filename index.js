b0e3c31eb75080ea2c224b8a608ff6631e7874c1
Samir Kumar <46542096+krsamir@users.noreply.github.com>
2022-04-22 21:33:45 +0530

    Looping and Array

---
+++ index.js
@@ -0,0 +1,24 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let index = 0; index < arr.length; index++) {
+//   console.log(arr[index]);
+// }
+
+const temp = arr.forEach((k) => k);
+// map, filter
+
+// arr.map((value) => {
+//   console.log(value);
+// });
+
+// const arr2 = [{ name: "Samir" }, { name: "Adi" }, { name: "nitish" }];
+// const nameArr = arr2.map((value) => value.name.toUpperCase());
+
+// console.log(nameArr);
+
+const abc = arr.filter((v, i) => v % 2 === 1);
+// const [first, second, third] = arr;
+const first = arr[0];
+const third = arr[2];
+console.log(abc);
+console.log(first, third);
